8f8966c3748eb70870fcfc79da0a4c4fcd395798
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-24 06:36:09 +0500

    oo

---
+++ src/app/products/page.tsx
@@ -0,0 +1,25 @@
+import BASE_PATH_FORAPI from "@/components/shared/BasePathForApi";
+import AllProductsCompo from "@/components/views/AllProducts";
+
+
+async function fetchAllProductData() {
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`, {
+        next: {
+            revalidate: 120
+        }
+    })
+    if (!res.ok) {
+        throw new Error("Failed to fetch product page")
+    }
+    return res.json();
+};
+
+
+const Products = async () => {
+    const ProdutcData = await fetchAllProductData()
+    return (
+        <AllProductsCompo ProdutcData={ProdutcData} />
+    )
+}
+
+export default Products
