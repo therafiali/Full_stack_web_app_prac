d4448d661758beebc7061709dc80942f32f4252b
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-24 05:05:24 +0500

    check

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
     const orignalData: Array<oneProductType> = [];
     const url = request.nextUrl.searchParams;
 
-    let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`);
+    let res = await fetch(`https://lt4uzorl.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`);
     let dataFrom_APi = await res.json();
     orignalData.push(...dataFrom_APi.result)
 
