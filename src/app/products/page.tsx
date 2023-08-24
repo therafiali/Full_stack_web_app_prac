8c0afc1e35f4f55bb4d83c4deb49ec17f2cbf0ac
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-24 05:12:41 +0500

    once

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,16 +3,17 @@ import AllProductsCompo from "@/components/views/AllProducts";
 
 
 async function fetchAllProductData() {
-    let res = await fetch(`https://my-app-pi-lake.vercel.app/api/products?start=0&end=10`, {
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`, {
         next: {
             revalidate: 120
         }
     })
     if (!res.ok) {
-        throw new Error("Failed to fetch product page")
+        throw new Error("Failed to fetch product data")
     }
     return res.json();
 };
+
 
 
 const Products = async () => {
