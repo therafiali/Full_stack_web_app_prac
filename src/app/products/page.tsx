0b655b6ee84a61dbf358cd67338fa8240fec5d61
Muhammad Rafi Ali <therafiali@gmail.com>
2023-08-24 05:18:10 +0500

    pull

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,11 +3,7 @@ import AllProductsCompo from "@/components/views/AllProducts";
 
 
 async function fetchAllProductData() {
-    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`, {
-        next: {
-            revalidate: 120
-        }
-    })
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`)
     if (!res.ok) {
         throw new Error("Failed to fetch product data")
     }
